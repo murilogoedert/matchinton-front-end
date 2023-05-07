a5a9844ed191362befe6dd70158ce52d108182be
murilogoedert <murilogoedert@gmail.com>
2023-05-06 22:05:13 -0300

    feat: add first view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+    import IconMatchintonLogo from '@/components/icons/IconMatchintonLogo.vue';
+</script>
+<template>
+    <div id="container-login">
+        <div id="white"></div>
+        <div id="red"></div>
+    </div>
+    <div id="form-overlay">
+        <IconMatchintonLogo/>
+    </div>
+</template>
+<style scoped>
+
+    #container-login{
+        display: grid;
+        grid-template-columns: 65% 35%;
+        height: 100%;
+        width: 100%;
+        position: relative;
+    }
+    #red{
+        background-color: var(--cor-vermelho);
+    }
+
+    #form-overlay{
+        background-color: rgba(0,0,0,0.1);
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        z-index: 1;
+        top: 0;
+    }
+
+</style>
